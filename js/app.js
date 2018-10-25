f25fd76a82cff41a1602bc1551dc78e8e2416129
djok92 <stefan.djokic92@gmail.com>
2018-10-25 18:37:42 +0200

    added loader

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
+//Loader
+setTimeout(function () {
+  $(".loader").fadeOut(500)
+}, 5000);
+
+
+//Scroll to Top
+$(window).scroll(function () {
+  if ($(this).scrollTop() >= 50 && ($(this).innerWidth() >= 550)) {
+    // If page is scrolled more than 50px
+    $(".btn-scroll-top").fadeIn(200); // Fade in the arrow
+  } else {
+    $(".btn-scroll-top").fadeOut(200); // Else fade out the arrow
+  }
+});
+$(".btn-scroll-top").click(function () {
+  // When arrow is clicked
+  $("body,html").animate({
+      scrollTop: 0 // Scroll to top of body
+    },
+    300
+  );
+});
+
 //Typewrite effect
 const TypeWriter = function (txtElement, words, wait = 1500) {
   this.txtElement = txtElement;
@@ -61,50 +85,31 @@ function init() {
   new TypeWriter(txtElement, words, wait);
 }
 
-//Scroll to top
-window.onscroll = function () {
-  const btn = document.querySelector(".btn-scroll-top");
-  if (
-    (document.body.scrollTop > 160 ||
-      document.documentElement.scrollTop > 160) && window.innerWidth > 550
-  ) {
-    btn.style.display = "block";
-    btn.addEventListener("click", () => {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
-    });
-  } else {
-    btn.style.display = "none";
-  }
-}
-
-
 function setClass(els, className, fnName) {
   for (let i = 0; i < els.length; i++) {
     els[i].classList[fnName](className);
-
   }
 }
 
 function accordion() {
   const acc = document.getElementsByClassName("accordion__button");
-  const panel = document.getElementsByClassName('accordion__content');
+  const panel = document.getElementsByClassName("accordion__content");
 
   for (let i = 0; i < acc.length; i++) {
     acc[i].onclick = function () {
-      let setClasses = !this.classList.contains('active');
-      setClass(acc, 'active', 'remove');
-      setClass(panel, 'show', 'remove');
-
+      let setClasses = !this.classList.contains("active");
+      setClass(acc, "active", "remove");
+      setClass(panel, "show", "remove");
 
       if (setClasses) {
         this.classList.toggle("active");
         this.nextElementSibling.classList.toggle("show");
-        this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + "px";
+        this.nextElementSibling.style.maxHeight =
+          this.nextElementSibling.scrollHeight + "px";
       } else {
         this.nextElementSibling.style.maxHeight = 0;
       }
-    }
+    };
   }
 }
 
